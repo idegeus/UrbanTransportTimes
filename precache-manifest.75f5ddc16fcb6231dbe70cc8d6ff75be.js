6c795bb07e08e5c18e57c6d5ee4360e4ae7b3e9a
Ivo de Geus <mail4ivo@gmail.com>
2020-07-08 15:19:40 +0200

    Updates

---
+++ precache-manifest.75f5ddc16fcb6231dbe70cc8d6ff75be.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771c7c6d7ab221e9c10ce8217d19de47",
+    "url": "/UrbanTransportTimes/index.html"
+  },
+  {
+    "revision": "a981edae0e1185957825",
+    "url": "/UrbanTransportTimes/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "7e799b9a84ba3540816b",
+    "url": "/UrbanTransportTimes/static/css/main.a4d955f5.chunk.css"
+  },
+  {
+    "revision": "a981edae0e1185957825",
+    "url": "/UrbanTransportTimes/static/js/2.2055cac4.chunk.js"
+  },
+  {
+    "revision": "27f79e8878249bae55f598a0b8e19d20",
+    "url": "/UrbanTransportTimes/static/js/2.2055cac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e799b9a84ba3540816b",
+    "url": "/UrbanTransportTimes/static/js/main.b642b10a.chunk.js"
+  },
+  {
+    "revision": "023ab917ee8b563c0429",
+    "url": "/UrbanTransportTimes/static/js/runtime-main.fcbbbfc3.js"
+  },
+  {
+    "revision": "18be30f9520dae4794748b96a166a8b1",
+    "url": "/UrbanTransportTimes/static/media/arrow-left.18be30f9.svg"
+  },
+  {
+    "revision": "805d6f57436327ef8e338166aea05413",
+    "url": "/UrbanTransportTimes/static/media/bike.805d6f57.svg"
+  },
+  {
+    "revision": "3b3b5dde91dd3581720cef2cd0092f6f",
+    "url": "/UrbanTransportTimes/static/media/car.3b3b5dde.svg"
+  },
+  {
+    "revision": "cd0b451d2c122d76a0da486b4ffd9269",
+    "url": "/UrbanTransportTimes/static/media/pt.cd0b451d.svg"
+  },
+  {
+    "revision": "73d11aa1452fc0f675b02c8cd3b89954",
+    "url": "/UrbanTransportTimes/static/media/walk.73d11aa1.svg"
+  }
+]);
