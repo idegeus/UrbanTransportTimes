c1fe0b92a6b54bec036c434b6099773099d5f678
Ivo de Geus <mail4ivo@gmail.com>
2023-06-05 16:59:22 +0200

    hopefully almost there

--- run.py
+++ run.py
@@ -1,16 +1,9 @@
 import os
-import time
-import pytz
-import requests
 import logging
 import pandas as pd
 import itertools
 import geopandas as gpd
-import sqlite3 as sl
-from shapely.geometry import Point, Polygon, MultiPolygon
-from shapely import wkt
-from datetime import datetime
-from timezonefinder import TimezoneFinder
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import docker
 import yaml
@@ -22,10 +15,9 @@ from util.extract_osm import extract_osm
 
 # Set logging and variables
 logging.getLogger().setLevel(logging.INFO) # DEBUG, INFO or WARN
-
-load_dotenv()
 DROOT = './1-data'
 CACHE = os.path.join(DROOT, '3-interim', 'graphhopper.db')
+load_dotenv()
 
 # Read a test city to be processed.
 cities = pd.read_excel(os.path.join(DROOT, '1-research', 'cities.xlsx'), index_col=0)
@@ -46,7 +38,7 @@ for pid, city in cities.iterrows():
     
     # Create OSM extracts
     osm_src = os.path.join(DROOT, '2-gh', 'osm', 'src', 'europe-latest.osm.pbf')
-    osm_out = os.path.join(DROOT, '2-gh', 'osm', 'out', '{city.ID_HDC_G0}.osm.pbf')
+    osm_out = os.path.join(DROOT, '2-gh', 'osm', 'out', f'{city.ID_HDC_G0}.osm.pbf')
     bbox = gdf.to_crs('EPSG:4326').unary_union
     extract_osm(osm_src, osm_out, bbox, buffer_m=5000, overwrite=False)
     
@@ -57,7 +49,7 @@ for pid, city in cities.iterrows():
     gtfs_out_dir = os.path.join(DROOT, '2-gh', 'gtfs')
     feed_paths = gtfs_client.download_feeds(feed_ids, gtfs_out_dir, city.ID_HDC_G0)
     
-    # Change configuration yaml file
+    # Update configuration yaml file
     config_src = os.path.join(DROOT, '2-gh', 'config-duttv2.src.yml')
     config_out = os.path.join(DROOT, '2-gh', 'config-duttv2.yml')
     with open(config_src, 'r') as f:
@@ -69,32 +61,59 @@ for pid, city in cities.iterrows():
     
     # Create Graphhopper based on created files.
     dclient = docker.from_env()
-    os.system('docker run -p 8989:8989 -d -v ${PWD}/1-data:/1-data --entrypoint /bin/bash israelhikingmap/graphhopper -c "java -Xmx10g -Xms10g -jar /graphhopper/*.jar server /1-data/2-gh/config-duttv2.yml"')
+    try:
+        graphhopper = dclient.containers.run(
+            image="israelhikingmap/graphhopper", 
+            detach=True,
+            command='"cd ../ && java -Xmx8g -Xms8g -jar ./graphhopper/*.jar server ./1-data/2-gh/config-duttv2.yml"',
+            environment={"JAVA_OPTS": "-Xmx8g -Xms8g"},
+            volumes={os.path.realpath(DROOT): {'bind': '/1-data', 'mode': 'rw'}}, 
+            entrypoint='/bin/bash -c',
+            ports={'8989/tcp':'8989/tcp'}
+        )
+        
+        # Wait for Graphhopper to actually finish preparing the graph.
+        start_ts = datetime.utcnow()
+        max_minutes = 10
+        for line in graphhopper.logs(stream=True):
+            line = str(line)
+            print(line)
+            logging.debug(line)
+            if "Starting GraphHopperApplication" in line:
+                logging.info(line)
+            if "Calendar range covered by all feeds" in line:
+                logging.info(line)
+            if "loaded graph" in line:
+                logging.info(line)
+            if datetime.utcnow() > (start_ts + timedelta(minutes=max_minutes)):
+                raise TimeoutError(f"Graphhopper took more than {max_minutes} minutes")
+            if "org.eclipse.jetty.server.Server - Started" in line:
+                logging.info("Graphhopper finished preparing, starting.")
+                break
     
-    # Wait for building to finish.
-    online = False
-    while not online:
-        try:
-            response = requests.get("http://localhost:8989/health")
-            if response.status_code == 200:
-                online = True
-        except:
-            logging.info("Docker is not online just yet, waiting 30 seconds more.")
-            pass
-            time.sleep(30)
+        # Check if container is built successfully and didn't stop yet.
+        if graphhopper.status == "exited":
+            raise RuntimeError("Graphhopper didn't load successfully, please check logs.")
+        
+        # Start querying Grasshopper. 
+        origins  = enumerate(gdf.centroid.to_crs("EPSG:4326"))
+        times    = [5, 15, 25, 35, 45]
+        modes_dt = [
+            # ('driving', 'driving-peak', datetime(2023, 6, 13, 8, 30, 37)), # Graphhopper doesn't do traffic.
+            # ('driving', 'driving-off',  datetime(2023, 6, 13, 8, 30, 37)), # Graphhopper doesn't do traffic.
+            ('driving', 'driving',      datetime(2023, 6, 13, 8, 30, 37)), 
+            ('transit', 'transit-peak', datetime(2023, 6, 13, 8, 30, 37)),
+            ('transit', 'transit',      datetime(2023, 6, 13, 13, 0, 37)), 
+            ('cycling', 'cycling',      datetime(2023, 6, 13, 13, 0, 37)), 
+            ('walking', 'walking',      datetime(2023, 6, 13, 8, 30, 37))
+        ]
 
-    # Start querying Grasshopper. 
-    origins  = enumerate(gdf.centroid.to_crs("EPSG:4326"))
-    times    = [5] # [5, 15, 25, 35, 45]
-    modes_dt = [
-        # ('driving', 'driving-peak', datetime(2023, 6, 13, 8, 30, 37)), # Graphhopper doesn't do traffic.
-        # ('driving', 'driving-off',  datetime(2023, 6, 13, 8, 30, 37)), # Graphhopper doesn't do traffic.
-        ('driving', 'driving',      datetime(2023, 6, 13, 8, 30, 37)), 
-        ('transit', 'transit-peak', datetime(2023, 6, 13, 8, 30, 37)),
-        ('transit', 'transit',      datetime(2023, 6, 13, 13, 0, 37)), 
-        ('cycling', 'cycling',      datetime(2023, 6, 13, 13, 0, 37)), 
-        ('walking', 'walking',      datetime(2023, 6, 13, 8, 30, 37))
-    ]
-
-    batch      = list(itertools.product(origins, times, modes_dt))
-    isochrones = client.get_isochrones(city.ID_HDC_G0, batch)
+        batch      = list(itertools.product(origins, times, modes_dt))
+        isochrones = client.get_isochrones(city.ID_HDC_G0, batch)
+        
+        
+    except:
+        raise RuntimeError("Graphhopper didn't load successfully, please check docs.")
+        pass
+    graphhopper.stop()
+    graphhopper.remove()
---
+++ util/readme.md
@@ -0,0 +1,4 @@
+# DUTTv2 Automation
+Software to automatically import population densities, GTFS feeds and mapping data in order to calculate transport & accessibility inequalities based on isochrones. 
+
+<img src='https://media.tenor.com/SSY2V0RrU3IAAAAM/rick-roll-rick-rolled.gif' width=400>
