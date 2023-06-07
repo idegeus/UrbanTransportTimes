5d4fc179fbaef14b102da90d663a5ff1cb8270b4
Ivo de Geus <mail4ivo@gmail.com>
2023-06-07 15:34:04 +0200

    chore: removed dockers for now

--- 2-dockers/osm_inator.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def main():
-    print('Nog gezelliger! OSM docker started. ')
- 
-if __name__ == "__main__":
-    main()
--- 2-dockers/popcenter_inator.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def main():
-    print('Het gezelligst! Popcenter docker started. ')
- 
-if __name__ == "__main__":
-    main()
