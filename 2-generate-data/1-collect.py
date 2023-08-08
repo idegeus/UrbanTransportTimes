be01f524f75321146dd072e86fd4bfe9ddf92502
Ivo de Geus <mail4ivo@gmail.com>
2023-08-08 13:23:56 +0200

    feat: added some more cities

--- 2-generate-data/1-collect.py
+++ 2-generate-data/1-collect.py
@@ -34,7 +34,7 @@ logging.getLogger().addHandler(console_handler)
 load_dotenv()
 
 # Read a test city to be processed.
-cities = pd.read_excel(os.path.join(DROOT, '1-research', 'cities.xlsx'))
+cities = pd.read_excel(os.path.join(DROOT, '1-research', 'cities.latest.xlsx'))
 # cities = cities[cities.city_name == 'Paris']
 logging.info(f"Total cities to be done: {cities.shape[0]}")
 
--- 2-generate-data/2-preprocess.py
+++ 2-generate-data/2-preprocess.py
@@ -17,7 +17,7 @@ from util.extract_urbancenter import ExtractCenters
 
 # Start processing cities.
 DROOT = '../1-data/'
-cities = pd.read_excel(os.path.join(DROOT, '1-research', 'cities.xlsx'))
+cities = pd.read_excel(os.path.join(DROOT, '1-research', 'cities.latest.xlsx'))
 CACHE = os.path.join(DROOT, '3-traveltime-cache', 'cache.main.v2.db')
 isochrone_client   = Isochrones(bing_key=os.environ['BING_API_KEY'], db=CACHE)
 urbancenter_client = ExtractCenters(src_dir=os.path.join(DROOT, '2-external'), 
@@ -31,8 +31,8 @@ for pid, city in cities.iterrows():
     pcl_path = urbancenter_client.extract_city(city.city_name, city.city_id)
     gdf = gpd.GeoDataFrame(pd.read_pickle(pcl_path))
 
-    peak_dt = datetime(2023, 6, 13, 8, 30, 0)
-    off_dt  = datetime(2023, 6, 13, 13, 30, 0)
+    peak_dt = datetime(2023, 8, 22, 8, 30, 0)
+    off_dt  = datetime(2023, 8, 22, 13, 30, 0)
     isochrone_config = [
         ('transit_off',        [15, 30], off_dt,  'g'),
         ('transit_peak',       [15, 30], peak_dt, 'g'),
